0687253872e68534235900f3917fef13c0430353
Aaron <aaron.petrini@hotmail.com>
2024-04-04 00:29:21 -0300

    Refactored2

---
+++ Source/GUI/PathProducer.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <JuceHeader.h>
+#include "AnalyzerPathGenerator.h"
+//#include "../DSP/SingleChannelSampleFifo.h"
+#include "Utilities.h"
+#include "FFTDataGenerator.h"
+#include "../PluginProcessor.h"
+
+
+struct PathProducer
+{
+    PathProducer(SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>& scsf) :
+    leftChannelFifo(&scsf)
+    {
+        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+    }
+    void process(juce::Rectangle<float> fftBounds, double sampleRate);
+    juce::Path getPath() { return leftChannelFFTPath; }
+private:
+    SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>* leftChannelFifo;
+    
+    juce::AudioBuffer<float> monoBuffer;
+    
+    FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+    
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+    
+    juce::Path leftChannelFFTPath;
+};
+
--- Source/GUI/Utilities.h
+++ Source/GUI/Utilities.h
@@ -11,6 +11,13 @@
 #pragma once
 #include <JuceHeader.h>
 
+enum FFTOrder
+{
+    order2048 = 11,
+    order4096 = 12,
+    order8192 = 13
+};
+
 template <
 typename Attachment,
 typename APVTS,
